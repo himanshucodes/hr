15f19a900162561d4296c7f8b882f27d13fba8b6
Himanshu Patel <himanshu.patel@gmail.com>
2023-10-28 12:38:17 -0400

    Initial commit.

---
+++ hr/src/main/java/hr1/CompetitiveGaming.java
@@ -0,0 +1,9 @@
+package hr1;
+
+import java.util.List;
+
+public class CompetitiveGaming {
+    public int numPlayers(int k, List<Integer> scores) {
+        throw new RuntimeException("Not implemented.");
+    }
+}
---
+++ hr/src/main/java/hr2/BandwidthDistribution.java
@@ -0,0 +1,9 @@
+package hr2;
+
+import java.util.List;
+
+public class BandwidthDistribution {
+    public long getMaxRequests(List<Integer>bandwidth, List<Integer> request, int total_bandwidth) {
+        throw new RuntimeException("Not implemented.");
+    }
+}
